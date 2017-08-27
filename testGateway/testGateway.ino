b0e7114bed4fa52e720bf858b5c88b54c19b9652
seksu <jarukit.suchat@gmail.com>
2017-08-27 15:07:46 +0700

    Add files via upload

---
+++ testGateway/testGateway.ino
@@ -0,0 +1,124 @@
+#include <ESP8266WiFi.h>
+// ERROR //
+// 101 can't connect
+// 102 can't receive data from node
+// 
+////////////////////// CONFIG ///////////////////////
+
+const char* ssid                  = "OHMAMP";
+const char* password              = "a029453745";
+
+const int rootId                  = 1;
+
+IPAddress ipaddr                  = {192, 168,   1,  2};
+IPAddress gateway                 = {192, 168,   1,  1};
+IPAddress subnet                  = {255, 255, 255,  0};
+
+////////////////////////////////////////////////////
+
+String strInput = "";
+String strNode[40] = "";
+IPAddress server_ip      = {192,168,1,3};
+const uint16_t port         = 8000;
+WiFiServer server(8000);
+WiFiClient client;
+const int maxIp = 4;
+int countAdd = 0;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  Serial.println();
+  Serial.print("Connecting to :");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(200);
+    Serial.print(".");
+  }
+  WiFi.config(ipaddr, gateway, subnet);
+
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  //server.begin();
+  WiFiClient client;
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void loop() {
+
+  //  client = server.available();
+  //  if (client) {
+  //    //Serial.println("new client");
+  //    for(int i=0 ; i<=100 ; i++){
+  //      if (client.available()) {
+  //        strInput = client.readStringUntil('\n');
+  //        Serial.println(strInput);
+  //        client.println(strInput.substring(0,9));
+  //        break;
+  //      }
+  //      delay(50);
+  //    }
+  //    client.stop();
+  //    digitalWrite(LED_BUILTIN,LOW);
+  //    delay(100);
+  //    digitalWrite(LED_BUILTIN,HIGH);
+  //    //Serial.println("client disconnect");
+  //  }
+  for (int ip = 3 ; ip <= maxIp ; ip++) {
+    server_ip[3] = ip;
+    Serial.print("connect to : 192.168.1.");
+    Serial.println(ip);
+    
+    int tmp = 0;
+    while(!client.connect(server_ip, port)) {
+      Serial.print(".");
+      delay(200);
+      tmp++;
+      if(tmp == 5){
+        Serial.println();
+        String strTmp = "Node:";
+        strTmp += ip-2; 
+        strTmp += ",101ERROR";
+        Serial.println(strTmp);
+        goto endfor;
+      }
+    }
+    client.print("require");
+    Serial.println("send require");
+    for (int i = 0 ; i <= 100 ; i++) {
+      if (client.available()) {
+        strInput = client.readStringUntil('\n');
+        strNode[countAdd] = strInput;
+        Serial.println(strNode[countAdd]);
+        countAdd++;
+        break;
+      }
+      if(i == 100){
+        String strTmp = "Node:"; 
+        strTmp += ip-2; 
+        strTmp += ",102ERROR"; //102ERROR is can't receive data from node
+        Serial.println(strTmp);
+        break; 
+      }
+      delay(50);
+    }
+    endfor:
+    client.stop();
+  }
+  
+  Serial.println("/////  data sumary /////");
+  for(int i=0 ; i<countAdd ; i++){
+    Serial.println(strNode[i]);
+  }
+  Serial.println(); 
+  countAdd = 0;
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(20000);
+}
---
+++ testNode/testNode.ino
@@ -0,0 +1,120 @@
+#include <ESP8266WiFi.h>
+// ssid : SmartFarmRouter
+// pass : SmartFarm00
+
+//////////////////// config ////////////////////////
+
+const char ssid[]           = "OHMAMP";
+const char password[]       = "a029453745";
+
+//const char server_ip[]      = "192.168.1.2";
+//const uint16_t port         = 8000;
+
+const int nodeId            = 3;
+
+IPAddress ipaddr            = {192, 168,   1,  3};
+IPAddress gateway           = {192, 168,   1,  1};
+IPAddress subnet            = {255, 255, 255,  0};
+
+///////////////////////////////////////////////////
+
+String strInput = "";
+#define MAXRAND 5
+
+WiFiServer server(8000);
+WiFiClient client;
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  Serial.println();
+  
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {        // wait for connect to AP
+    delay(200);
+    Serial.print(".");
+  }
+  WiFi.config(ipaddr, gateway, subnet);
+  
+  Serial.println();
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+  digitalWrite(LED_BUILTIN,HIGH);
+}
+void loop() {
+//  long tempRand = random(1,MAXRAND);
+//  Serial.print("RAND : ");
+//  Serial.println(tempRand);
+//  delay(1000 * tempRand);
+//
+//  while (!client.connect(server_ip, port))
+//  Serial.print(".");
+//
+//  String temp = "&nodeId=";
+//  temp += nodeId;
+//  temp += "&data1=";
+//  temp += 1;
+//  temp += "&data2=";
+//  temp += 2;
+//  temp += "&data3=";
+//  temp += 3;  
+//  client.println(temp);
+//  Serial.println("Sended");
+//
+//  for(int i=0,max=15 ; i<=max ; i++){
+//    if(client.available()){
+//      strInput = client.readStringUntil('\n');
+//      Serial.println(strInput.charAt(8)-48);
+//      if(strInput.charAt(8)-48 == nodeId){
+//        Serial.println("NodeId is right");
+//        break;
+//      }
+//      else
+//        return;
+//    }
+//    if(i == max)
+//      return;
+//    delay(200);
+//  }
+//  client.stop();
+//  digitalWrite(LED_BUILTIN,LOW);
+//  delay(500);
+//  digitalWrite(LED_BUILTIN,HIGH);
+//  delay(20000);
+
+  client = server.available();
+  if (client) {
+    //client.println("pass");
+    for(int i=0 ; i<100 ; i++){
+      if(client.available()) {
+        strInput = client.readStringUntil('\n');
+        Serial.println(strInput);
+        if(strInput == "require"){
+          client.println(sensorRead());
+          return;
+        }
+      }
+      delay(50);
+    }
+    client.stop();
+    digitalWrite(LED_BUILTIN,LOW);
+    delay(100);
+    digitalWrite(LED_BUILTIN,HIGH); 
+  }
+}
+
+String sensorRead(){
+  String temp = "19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,  19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,  19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,  19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,  19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,  19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,  19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,  19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,  19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,  19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,  19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,  19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,  19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,  19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,  19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,  19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,  19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,  19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00,19 Oct 2016 22:35:50,18,5,62,31.00,47.00,30.00";
+  
+//  temp += nodeId;
+//  temp += "&data1=";
+//  temp += 1;
+//  temp += "&data2=";
+//  temp += 2;
+//  temp += "&data3=";
+//  temp += 3;  
+  return temp;
+}
